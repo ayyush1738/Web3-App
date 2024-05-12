08a740373f91c77780242f4e156cebdd4cef0aa4
ayyush1738 <singhrathoreayush824@gmial.com>
2024-05-13 01:45:58 +0530

    Dapp

--- client/src/components/Swap.jsx
+++ client/src/components/Swap.jsx
@@ -1,23 +1,63 @@
-import react from 'react';
+import React, { useState, useEffect } from "react";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import tokenList from "../tokenList.json"
+import axios from "axios";
+import { useSendTransaction, useWaitForTransaction } from "wagmi";
 
-export default function Swap()
-{
-    return(
-        <>
-        <div className="tradeBox m-auto bg-purple-700 h-80 w-96 rounded-2xl">
-            <h4 className='p-2 text-2xl text-white'>Swap</h4>
-            <div className='h-28  bg-purple-400 m-2 rounded-2xl p-2'>
-                <section className='text-gray-500'>You Pay</section>
-                <input className="text-4xl border-none outline-none placeholder-white w-20 bg-transparent" type="text" placeholder='0'/>
-                <button className="text-2xl text-purple-700 rounded-2xl float-right bg-white w-28">ETH</button>
-            </div>
-            <div className='h-28 bg-purple-400 m-2 p-2 rounded-2xl'>
-                <section className='text-gray-500'>You Recieve</section>
-                <input className="text-4xl border-none outline-none placeholder-white w-20 bg-transparent" type="text" placeholder='0'/>
-                <button className="text-2xl text-purple-700 rounded-2xl float-right bg-white w-28">ETH</button>
-            </div>
-            <button className=' bg-purple-400 rounded-2xl p-2 m-auto'>Connect</button>
-        </div>
-        </>
-    )
+
+function Swap(props) {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+
+  function handleSlippageChange()
+  {
+    setSlippage(e.target.value);
+  }
+
+  function changeAmount(e){
+    setTokenOneAmount(e.target.value);
+  }
+
+  const settings=(
+    <>
+    <div>Slippage Tolerance</div>
+    <div>
+      <Radio.Group value={slippage} onChange={handleSlippageChange}>
+        <Radio.Button value={0.5}>0.5%</Radio.Button>
+        <Radio.Button value={0.5}>2.5%</Radio.Button>
+        <Radio.Button value={5}>5.0%</Radio.Button>
+      </Radio.Group>
+    </div>
+    </>
+  )
+
+  return(
+    <>
+    <div className="tradeBox w-96 h-96 m-auto bg-purple-600 rounded-2xl">
+      <div className="tradeBoxHeader flex justify-between">
+        <h4 className="m-2 text-white">Swap</h4>
+        <Popover
+          content={settings}
+          title="Settings"
+          trigger="click"
+          className="m-2 text-white"
+        >
+          <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+      <div className="inputs">
+        <Input placeholder="0" value={tokenOneAmount} onchange={changeAmount}/>
+        <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+      </div>
+    </div>
+    </>
+  )
 }
+
+export default Swap;
